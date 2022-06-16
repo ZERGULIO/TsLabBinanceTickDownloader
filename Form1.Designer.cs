e82d274f4da3fbab52ede1dda5bac04d75311ede
Сергей Дземин <Gambit501@yandex.ru>
2022-06-16 21:29:11 +1000

    Последнее

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,12 +37,23 @@
             this.label_coin_selected = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.logbox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -105,13 +116,12 @@
             // label_coin_selected
             // 
             this.label_coin_selected.AutoSize = true;
-            this.label_coin_selected.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_coin_selected.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label_coin_selected.Location = new System.Drawing.Point(60, 19);
             this.label_coin_selected.Name = "label_coin_selected";
-            this.label_coin_selected.Size = new System.Drawing.Size(131, 21);
+            this.label_coin_selected.Size = new System.Drawing.Size(138, 21);
             this.label_coin_selected.TabIndex = 12;
             this.label_coin_selected.Text = "Тикер не выбран";
-            this.label_coin_selected.Click += new System.EventHandler(this.label_coin_selected_Click);
             // 
             // pictureBox1
             // 
@@ -126,6 +136,15 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label_coin_selected);
@@ -135,14 +154,114 @@
             this.groupBox2.TabIndex = 14;
             this.groupBox2.TabStop = false;
             // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(214, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 99);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Внимание, в настройках поставщика в TsLab выставите Глубину загрузки тиков не мен" +
+    "ее 3000 минут.\r\n";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 41);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Папка с готовыми Тиками";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(223, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 41);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Папка Кеш - TsLab";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Кол-во дней";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(407, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "--------------------------------------------------------------------------------";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(17, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 28);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Загрузить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 127);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(131, 23);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(17, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Загрузка истории";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(6, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 19);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Доступно истории:";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(82, 155);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(133, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(15, 19);
             this.label1.TabIndex = 14;
-            this.label1.Text = "label1";
+            this.label1.Text = "-";
             // 
             // label4
             // 
@@ -154,15 +273,26 @@
             this.label4.TabIndex = 13;
             this.label4.Text = "Ticker:";
             // 
+            // logbox
+            // 
+            this.logbox.BackColor = System.Drawing.Color.Gainsboro;
+            this.logbox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.logbox.Location = new System.Drawing.Point(12, 322);
+            this.logbox.Multiline = true;
+            this.logbox.Name = "logbox";
+            this.logbox.Size = new System.Drawing.Size(543, 157);
+            this.logbox.TabIndex = 16;
+            // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
             this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(12, 288);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 15;
             this.button1.Text = "Выбрать";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
@@ -171,12 +301,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(567, 491);
+            this.Controls.Add(this.logbox);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -185,6 +319,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,5 +337,15 @@
         private Button button1;
         private Label label1;
         public Label label_coin_selected;
+        private TextBox logbox;
+        private Label label2;
+        private Button button2;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+        private Label label5;
+        private Label label6;
+        private Button button4;
+        private Button button3;
+        private Label label7;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,10 @@
+using System.Diagnostics;
 using System.Net;
 using System.Xml;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace TsLabBinanceTickDownloader
 {
@@ -9,22 +14,37 @@ namespace TsLabBinanceTickDownloader
         public string url_spot = "https://s3-ap-northeast-1.amazonaws.com/data.binance.vision?delimiter=/&prefix=data/spot/daily/aggTrades/";
         List<string> list_arhives = new List<string>();
         public string coin_type = "";
+        public string Core_Folder = Directory.GetCurrentDirectory() + @"\Core\";
+        public string Temp_Folder = Directory.GetCurrentDirectory() + @"\Core\Temp_Folder\";
+        static List<string> new_list = new List<string>();
+        string version = "1.0";
+
         public Form1()
         {
             InitializeComponent();
+            Folder_Work();
+            this.Text = "TSBDT ver." + version;
         }
         public static List<string> arr_coin = new List<string>() { };
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Log("Выберите - Тип рынка - в верхнем левом углу");
         }
         public string Get_Html_Code(string url)
         {
-            string response;
+            string response = "";
             using (var webClient = new WebClient())
             {
+                webClient.Proxy = null;
                 // Выполняем запрос по адресу и получаем ответ в виде строки
-                response = webClient.DownloadString(url);
+                try
+                {
+                    response = webClient.DownloadString(url);
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
             }
             return response;
         }
@@ -68,7 +88,7 @@ namespace TsLabBinanceTickDownloader
                     string text = element.GetElementsByTagName("Key")[0].InnerText;
                     if(text.IndexOf("CHECKSUM") == -1)
                     {
-                        list_arhives.Add(text);
+                        list_arhives.Add("https://data.binance.vision/" + text);
                     }
                 }
             }
@@ -85,7 +105,7 @@ namespace TsLabBinanceTickDownloader
                 }
             }
         }
-        public void listbupdate()
+        public  void listbupdate()
         {
             listBox1.Items.Clear();
             foreach (var item in arr_coin)
@@ -97,7 +117,8 @@ namespace TsLabBinanceTickDownloader
         }
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            button1.Enabled = false;
+            logbox.Clear();
+            Log("Выбран тип рынка - Futures");
             coin_type = "futures";
             listBox1.Enabled = false;
             listBox1.BackColor = Color.LightGray;
@@ -107,10 +128,12 @@ namespace TsLabBinanceTickDownloader
             listBox1.BackColor = Color.White;
             textBox1.Enabled = true;
             listbupdate();
+            Log("Загружено инструментов: " + arr_coin.Count.ToString());
         }
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            button1.Enabled = false;
+            logbox.Clear();
+            Log("Выбран тип рынка - Spot");
             coin_type = "spot";
             listBox1.Enabled = false;
             listBox1.BackColor = Color.LightGray;
@@ -120,25 +143,26 @@ namespace TsLabBinanceTickDownloader
             listBox1.BackColor = Color.White;
             textBox1.Enabled = true;
             listbupdate();
+            Log("Загружено инструментов: " + arr_coin.Count.ToString());
         }
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-        }
-        private void label_coin_selected_Click(object sender, EventArgs e)
-        {
-
+            button1.Enabled = true;
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            numericUpDown1.Enabled = false;
+            button2.Enabled = false;
             button1.Text = "Загрузка";
+            button1.BackColor = Color.IndianRed;
             button1.Enabled = false;
             coin_setup(listBox1.Text);
             button1.Enabled = true;
             label_coin_selected.Text = listBox1.Text;
             button1.Text = "Выбрать";
+            button1.BackColor = Color.Gainsboro;
         }
-        public void coin_setup(string coin_name)
+        public  void coin_setup(string coin_name) // Информация об Инструменте кол-во ит.д
         {
             string url_fut_arh = "https://s3-ap-northeast-1.amazonaws.com/data.binance.vision?delimiter=/&prefix=data/futures/um/daily/aggTrades/";
             string url_spot_arh = "https://s3-ap-northeast-1.amazonaws.com/data.binance.vision?delimiter=/&prefix=data/spot/daily/aggTrades/";
@@ -154,12 +178,262 @@ namespace TsLabBinanceTickDownloader
 
             xml_get_info(Get_Html_Code(url), "list_arhives");
 
-            label1.Text = list_arhives.Count.ToString();
+            label1.Text = list_arhives.Count.ToString() + " дней";
+            numericUpDown1.Maximum = list_arhives.Count();
 
+            foreach (var item in list_arhives)
+            {
+                //Вывод массива ссылок на архивы
+            }
+            Download_Setup(coin_name);
+
+        }
+        List<string> download_url = new List<string>();
+        public void Download_Setup(string coin_name) // Генерация массива ссылок для последующей скачки их
+        {
+            string date(int i)
+            {
+                DateTime dateTime = DateTime.Today.Date;
+                return dateTime.AddDays(-i).ToString("yyyy-MM-dd");
+
+            }
+            numericUpDown1.Enabled = true;
+            button2.Enabled = true;
+            if(coin_type == "futures")
+            {
+                download_url.Clear();
+                for (int i = 1; i < 502; i++)
+                {
+                    download_url.Add("https://data.binance.vision/data/futures/um/daily/aggTrades/" + coin_name + "/" + coin_name + "-aggTrades-" + date(i) + ".zip");
+                    //Log(download_url[i-1]);
+                }
+            }
+            if (coin_type == "spot")
+            {
+                download_url.Clear();
+                for (int i = 1; i < 502; i++)
+                {
+                    download_url.Add("https://data.binance.vision/data/spot/daily/aggTrades/" + coin_name + "/" + coin_name + "-aggTrades-" + date(i) + ".zip");
+                    //Log(download_url[i - 1]);
+                }
+            }
+            
+        }
+        public async Task Download_File(int max)
+        {
+            block();
+            int index = 0;
+            string get_url()
+            {
+
+                 string url = download_url[index];
+                 index++;
+                 return url;
+
+            }
+            var tasks = new List<Task>();
+            int dow = 0;
+            for (var i = 0; i < 3; i++)
+            {
+                tasks.Add(Task.Run(() => {
+                    while (true) 
+                    {
+                        dow++;
+                        using (var client = new WebClient())
+                        {
+                            WebRequest.DefaultWebProxy = null;
+                            try
+                            {
+                                string url = get_url();
+                                if(dow > max) { break; }
+                                string filename = Path.GetFileName(url);
+                                client.DownloadFile(url, Temp_Folder + filename);
+                                Log("Download + " + filename);
+                            }
+                            catch (Exception e)
+                            {
+                                dow--;
+                                continue;
+                            }
+                        }
+                    }
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Log("Завершили загрузку архивов");
+            Extract_Arhives();
         }
         private void textBox1_Click(object sender, EventArgs e)
         {
             textBox1.Text = "";
+        }
+        public void Log(string text) // Вывод в Log
+        {
+            logbox.AppendText(text + Environment.NewLine);
+        }
+        private void button3_Click(object sender, EventArgs e) // Папка Tslab с кешами в AppData
+        {
+            Process.Start("explorer", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+        }
+        private void button4_Click(object sender, EventArgs e) // Папка с Тиками
+        {
+            Process.Start("explorer", Directory.GetCurrentDirectory() + @"\Core\_result");
+        }
+        private void button2_Click(object sender, EventArgs e) // Кнопка начала - Загрузки
+        {
+            Log("Запуск загрузки - Кол-во дней истории - " + numericUpDown1.Value);
+            Download_File((int)numericUpDown1.Value);
+        }
+        public void block()
+        {
+            groupBox2.Enabled = false;
+            button1.Enabled = false;
+            listBox1.Enabled = false;
+            textBox1.Enabled = false;
+            groupBox1.Enabled = false;
+        }
+        public void unlock()
+        {
+            groupBox2.Enabled = true;
+            button1.Enabled = true;
+            listBox1.Enabled = true;
+            textBox1.Enabled = true;
+            groupBox1.Enabled = true;
+        }
+        public void Folder_Work() // При запуске проверяет наличие нужных папок для работы
+        {
+            try
+            {
+                if (Directory.Exists(Core_Folder))
+                {
+                    if (Directory.Exists(Core_Folder + "_result"))
+                    {
+
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(Core_Folder + "_result");
+                    }
+                    if (Directory.Exists(Temp_Folder))
+                    {
+                        Directory.Delete(Temp_Folder, true);
+                        Directory.CreateDirectory(Temp_Folder);
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(Temp_Folder);
+                    }
+                }
+                else
+                {
+                    Log("Прога Работать не будет. ВЫ СЛОМАЛИ ЕЁ.... СКАЧАЙТЕ НОРМАЛЬНУЮ СБОРКУ С GITHUBA....");
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        public void Extract_Arhives()
+        {
+            string[] arr_file = Directory.GetFiles(Temp_Folder);
+            foreach (var item in arr_file)
+            {
+
+                Log("Разархивирование - " + Path.GetFileName(item));
+                ZipFile.ExtractToDirectory(item, Temp_Folder);
+                File.Delete(item);
+            }
+            string[] arr_csv = Directory.GetFiles(Temp_Folder);
+            foreach (var item in arr_csv)
+            {
+                Read_CSV(item, Path.GetFileName(item));
+            }
+            StartCmdBatch();
+        }
+        public void Read_CSV(string path, string namefile) // Чтения CSV файлов
+        {
+
+            // чтение из файла
+            try
+            {
+                string line;
+                new_list.Clear();
+                new_list.Add("<TRADENO>,<LAST>,<VOL>,<DATE>,<TIME>,<OPER>");
+                using (StreamReader f = new StreamReader(path))
+                {
+                    while ((line = f.ReadLine()) != null)
+                    {
+                        Converting(ref line);
+                    }
+                }
+                //Console.WriteLine("Заполнили массив");
+                Write_File(new_list, namefile);  // Записываем в файл готовые тики
+                Log("Конвертирование - " + namefile);
+
+            }
+            catch (Exception e)
+            {
+                Log(e.ToString());
+            }
+
+        }
+        public void Converting(ref string line) // Конвертирование данных в нужный формат тиков для TsLab
+        {
+            string TRADENO, LAST, VOL, DATE, TIME, OPER;
+
+            string[] l = line.Split(',');
+            TRADENO = l[0];
+            LAST = l[1];
+            VOL = l[2];
+            DATE = Get_Date(Convert.ToDouble(l[5].Remove(l[5].Length - 3)));
+            TIME = Get_Time(Convert.ToDouble(l[5].Remove(l[5].Length - 3)));
+            if (l[6] == "true") { OPER = "B"; }
+            else { OPER = "S"; }
+            string good = TRADENO + "," + LAST + "," + VOL + "," + DATE + "," + TIME + "," + OPER;
+
+            new_list.Add(good);
+
+            static string Get_Date(double unixTimeStamp)
+            {
+                DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                dateTime = dateTime.AddSeconds(unixTimeStamp);
+                return dateTime.ToString("yyyyMMdd");
+            }
+            static string Get_Time(double unixTimeStamp)
+            {
+                DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                dateTime = dateTime.AddSeconds(unixTimeStamp);
+                return dateTime.ToString("HHmmss");
+            }
+        }
+        public void Write_File(List<string> new_list, string namefile)
+        {
+            using (StreamWriter w = new StreamWriter(Temp_Folder + namefile.Replace(".csv", ".txt"), false))
+            {
+
+                foreach (var item in new_list)
+                {
+                    w.WriteLine(item);
+                }
+                File.Delete(Temp_Folder + "/" + namefile);
+
+            }
+        }
+        public void StartCmdBatch()
+        {
+            string[] arr_file = Directory.GetFiles(Temp_Folder);
+            using (StreamWriter w = new StreamWriter(Core_Folder + "run.bat", false, Encoding.Default))
+            {
+                w.WriteLine("cd Core");
+                foreach (var item in arr_file)
+                {
+                    w.WriteLine("ConvertTicks.exe " + label_coin_selected.Text + " \"" + item + "\" \\c");
+                }
+            }
+            string parameters = String.Format("/k \"{0}\"", Core_Folder + "run.bat");
+            System.Diagnostics.Process.Start("cmd", parameters);
+            Environment.Exit(0);
         }
     }
 }
